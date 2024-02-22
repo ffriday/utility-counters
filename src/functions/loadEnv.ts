779b1762eda7c3bd09dcf8e66cd4e32a05aafd3f
mitrich <4irtim@gmail.com>
2024-02-22 22:33:31 +0700

    feat: add env config loading

---
+++ src/functions/loadEnv.ts
@@ -0,0 +1,19 @@
+export const loadEnv = () => {
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  };
+
+  const envNotLoaded = Object.values(firebaseConfig)
+    .filter((value) => value === undefined)
+    .length > 0;
+
+  if(envNotLoaded) throw new Error('config from .env file not loaded')
+
+  return firebaseConfig;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { loadEnv } from './functions'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+console.log(loadEnv());
 
 app.mount('#app')
